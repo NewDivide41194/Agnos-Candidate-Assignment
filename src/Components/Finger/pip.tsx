7e55075900c320d445e33ce016cc3a785227c204
HeinHtet41194 <heinhtet25944@gmail.com>
2024-09-09 22:41:15 +0700

    fix pip alignment

--- src/Components/Finger/pip.tsx
+++ src/Components/Finger/pip.tsx
@@ -11,7 +11,7 @@ const FingerPip = (props: FingerPipProps): JSX.Element => {
     const { isShowHighlightPip, isSelectedAll, _handleOnClickPip } = props;
     return (
         <>
-            <div className="w-100 z-20 absolute left-[11.5%] top-[25.8%] w-[71%]"
+            <div className="w-100 z-20 absolute left-[11%] top-[25.8%] w-[71%]"
                 onClick={_handleOnClickPip}>
                 {!isSelectedAll && <div className="relative">
                     <div className="absolute top-[-25px] left-[145px] w-[130px]">
